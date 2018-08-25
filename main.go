fd8d5fc4e14791faa767d96d5f8ece42a73f9d32
cpluspluscrowe <ccrowe@unomaha.edu>
2018-08-25 19:04:32 +0000

    added cursor for finding older friends

--- main.go
+++ main.go
@@ -16,28 +16,57 @@ func doEvery(d time.Duration, f func()) {
 }
 
 func main() {
-	doEvery(600*time.Second, UnfollowAUserNotFollowingMe)
+	UnfollowAUserNotFollowingMe()
+	doEvery(86400*time.Second, UnfollowAUserNotFollowingMe)
+}
+
+func GetNextFriendsCursor(client *twitter.Client, cursor int64) int64 {
+	var friendParams *twitter.FriendListParams
+	if cursor > -1 {
+		friendParams = &twitter.FriendListParams{}
+	} else {
+		friendParams = &twitter.FriendListParams{Cursor: cursor}
+	}
+	friends, _, err := client.Friends.List(friendParams)
+	if err != nil {
+		panic(err)
+	}
+	friendsData := friends
+	nextCursor := friendsData.NextCursor
+	return nextCursor
+}
+
+func GetOlderFriends(client *twitter.Client) []twitter.User {
+	var cursor int64 = -1
+	for i := 0; i < 4; i++ {
+		cursor = GetNextFriendsCursor(client, cursor)
+		time.Sleep(10 * time.Millisecond)
+		fmt.Println(cursor)
+	}
+	friendParams := &twitter.FriendListParams{Cursor: cursor}
+	friends, _, err := client.Friends.List(friendParams)
+	if err != nil {
+		panic(err)
+	}
+	users := friends.Users
+	return users
 }
 
 func UnfollowAUserNotFollowingMe() {
 	client := getTwitterClient()
-	friends, _, err := client.Friends.List(&twitter.FriendListParams{Count: 1000})
-	users := friends.Users
-	if err != nil {
-		panic(err)
+	users := GetOlderFriends(client)
+	for _, user := range users {
+		relationship, _, err := client.Friendships.Show(&twitter.FriendshipShowParams{TargetID: user.ID})
+		if err != nil {
+			panic(err)
+		}
+		if !relationship.Target.Following {
+			fmt.Println("Unfollowing: ", relationship.Target)
+			client.Friendships.Destroy(&twitter.FriendshipDestroyParams{UserID: user.ID})
+		}
+		time.Sleep(100 * time.Millisecond)
 	}
-	user := users[len(users)-1:][0]
-	//for _, user := range users {
-	relationship, _, err := client.Friendships.Show(&twitter.FriendshipShowParams{TargetID: user.ID})
-	if err != nil {
-		panic(err)
-	}
-	if !relationship.Target.Following {
-		fmt.Println("Unfollowing: ", relationship.Target)
-		client.Friendships.Destroy(&twitter.FriendshipDestroyParams{UserID: user.ID})
-	}
-	//}
-
+	fmt.Println("Finished unfollowing method")
 }
 
 func getClient() *http.Client {
